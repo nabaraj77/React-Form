6687ece46f59f3b5c6654df413add702315996a5
Nabaraj <nabaraj.dhl77@gmail.com>
2022-06-21 20:55:52 +0545

    form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [formData, setFormData] = useState({
+    fName: "",
+    lName: "",
+    email: "",
+    age: "",
+    comments: "",
+    isFriendly: true,
+  });
+  console.log(formData);
+  const formDataHandler = (e) => {
+    // Destructuring of object
+    const { name, value, type, checked } = e.target;
+    setFormData((formPreviousData) => {
+      return {
+        ...formPreviousData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="fName"
+        name="fName"
+        onChange={formDataHandler}
+        value={formData.fName}
+      />
+      <input
+        type="text"
+        name="lName"
+        id=""
+        placeholder="lName"
+        onChange={formDataHandler}
+        value={formData.lName}
+      />
+      <input
+        type="number"
+        name="age"
+        id=""
+        placeholder="Age"
+        onChange={formDataHandler}
+        value={formData.age}
+      />
+      <input
+        type="email"
+        name="email"
+        id=""
+        placeholder="Email"
+        onChange={formDataHandler}
+        value={formData.email}
+      />
+      <textarea
+        name="comments"
+        id=""
+        cols="30"
+        rows="10"
+        value={formData.comments}
+        onChange={formDataHandler}
+      />
+      {/* //CHECKBOXES */}
+      <input
+        type="checkbox"
+        name="isFriendly"
+        id="isFriendly"
+        checked={formData.isFriendly}
+        onChange={formDataHandler}
+      />
+      <label htmlFor="isFriendly">Are you Friendly</label>
+      <br />
     </div>
   );
-}
+};
 
 export default App;
